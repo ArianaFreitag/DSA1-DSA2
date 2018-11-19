f67e72d89b244bf23b390430a1e5d7e39605f0e1
Ariana Freitag <ariana@Arianas-MBP.home>
2018-11-18 21:25:46 -0500

    added in pset 4

---
+++ Djikstra/djikstra.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "graph.h"
+
+int main() {
+  std::string start;
+  std::string inFile, outPath;
+  std::string line;
+  graph gph;
+
+  std::cout << "\nEnter name of graph file: ";
+  std::cin >> inFile;
+  std::ifstream graphList (inFile.c_str());
+  if (graphList.is_open()) {
+    while (std::getline(graphList, line)) {
+      gph.insert(line);
+    }
+    graphList.close();
+  }
+
+  while (!gph.nodeExists(start)) {
+    std::cout << "Enter a valid vertix ID for the starting vertex: ";
+    std::cin >> start;
+  }
+
+  clock_t begin = clock();
+  gph.dijkstra(start);
+  clock_t end = clock();
+  double elapsed_secs = ((double)(end-begin)) / CLOCKS_PER_SEC;
+  std::cout << "Total time (in seconds) to apply dijkstra's algorithm: " << elapsed_secs << "\n";
+  std::cout << "Enter name of output file: ";
+  std::cin >> outPath;
+  std::ofstream outFile (outPath.c_str());
+  if(!outFile) {
+    std::cout << "Error: could not open " << outPath << "\n";
+  }
+
+  gph.returnGraph(outFile);
+  outFile.close();
+  return 0;
+}
---
+++ Djikstra/graph.h
@@ -0,0 +1,60 @@
+#ifndef _GRAPH_H
+#define _GRAPH_H
+
+#include <fstream>
+#include <vector>
+#include <list>
+#include <string>
+#include <climits>
+#include "heap.h"
+
+class graph {
+
+  public:
+    graph();
+    // create a graph with a new hashTable
+
+    bool dijkstra(const std::string &start);
+    // apply dijkstra's algorithm on a graph
+    // pass in the starting point
+
+    bool nodeExists(const std::string &node);
+    // check if node has been found
+    // pass in node to find
+
+    void insert(const std::string &line);
+    // insert the node into graph
+    // pass in line of input file
+
+    void returnGraph(std::ofstream &out);
+    // output the graph
+    // pass in output file path
+
+  private:
+    int capacity; // size of graph
+    class edge;
+    class node;
+
+    class edge {
+      public:
+        int cost;  // edge cost
+        node *dest;  // destination node
+    };
+
+    class node {
+      public:
+        std::string name;  // name of vertix
+        std::list<edge> adj;  // list full of edges from node
+        // for djikstra
+        bool known; // if node has been found
+        int dist; // distance
+        std::list<std::string> path;
+    };
+
+    // list of nodes
+    std::list<node*> nodeList;
+    // hashtable of nodes
+    hashTable * graphHashTable;
+};
+
+#endif
